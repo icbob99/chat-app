33e626b9931427d9e22729eff72421075aeadddd
Boris <boris.shulman@tabit.cloud>
2021-09-12 11:15:23 +0300

    add start dev to packge

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const path = require('path')
+
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
-app.get('', (req, res) => {
-    // res.send('Hello express!')
-    res.render('index')
-})
+// app.get('', (req, res) => {
+//     // res.send('Hello express!')
+//     res.render('index')
+// })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
